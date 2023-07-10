6a9b042bb058300f703af7c804e711138f362ddb
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-07-10 21:13:58 +0100

    implemented bootstrap stylings to 'App' component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App h-100 d-flex flex-column align-items-center container-fluid justify-content-start">
       
     </div>
   );
