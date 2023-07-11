a6eca57221c2ae36311ea142b5eb97747b7464ae
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-07-11 20:46:42 +0100

    implemented 'GatherAndGatherLogo' component into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import GatherAndGatherLogo from './components/GatherAndGatherLogo/GatherAndGatherLogo';
 
 function App() {
   return (
     <div className="App h-100 d-flex flex-column align-items-center container-fluid justify-content-start">
-      
+      <GatherAndGatherLogo />
     </div>
   );
 }
