fd72f78d5e98c6667d686570acb1881a10a1c0e1
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-07-18 21:49:38 +0100

    imported 'RosterHoursTable' component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import GatherAndGatherLogo from './components/GatherAndGatherLogo/GatherAndGatherLogo';
+import RosterHoursTable from './components/RosterHoursTable/RosterHoursTable';
 
 function App() {
   return (
-    <div className="App h-100 d-flex flex-column align-items-center container-fluid justify-content-start">
+    <div className="App h-100 d-flex flex-column align-items-center container-fluid">
       <GatherAndGatherLogo />
+      <RosterHoursTable />
     </div>
   );
 }
