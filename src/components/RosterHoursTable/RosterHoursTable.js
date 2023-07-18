8864451f61597e05f184acaff3ab9d4b432dbfcd
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-07-18 21:52:45 +0100

    started work on Roster Table component

---
+++ src/components/RosterHoursTable/RosterHoursTable.js
@@ -0,0 +1,11 @@
+import './RosterHoursTable.css'
+
+const RosterHoursTable = () => {
+    return (
+        <table className='mt-3 h-100 w-100 bg-primary'>
+
+        </table>
+    )
+}
+
+export default RosterHoursTable;
