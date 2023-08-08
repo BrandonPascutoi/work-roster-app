f158323aa36cd446d4ea616341c9062fe73cf3a3
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-08-08 21:04:38 +0100

    created basic design for roster. Needs to be modified to fit proportions correctly

--- src/components/RosterHoursTable/RosterHoursTable.js
+++ src/components/RosterHoursTable/RosterHoursTable.js
@@ -2,8 +2,204 @@ import './RosterHoursTable.css'
 
 const RosterHoursTable = () => {
     return (
-        <table className='mt-3 h-100 w-100 bg-primary'>
-
+        <table className='mt-3 h-100 w-100 table-bordered'>
+            <tbody>
+                <tr>
+                    <th colSpan={3} rowSpan={4}>Employee Information</th>
+                    <th colSpan={2} rowSpan={3}>Monday</th>
+                    <th colSpan={2} rowSpan={3}>Tuesday</th>
+                    <th colSpan={2} rowSpan={3}>Wednesday</th>
+                    <th colSpan={2} rowSpan={3}>Thursday</th>
+                    <th colSpan={2} rowSpan={3}>Friday</th>
+                    <th colSpan={2} rowSpan={3}>Saturday</th>
+                    <th colSpan={2} rowSpan={3}>Sunday</th>
+                    <th colSpan={3} rowSpan={6}>Total Work Hours</th>
+                </tr>
+                <tr></tr>
+                <tr></tr>
+                <tr>
+                    <td colSpan={2}></td>
+                    <td colSpan={2}></td>
+                    <td colSpan={2}></td>
+                    <td colSpan={2}></td>
+                    <td colSpan={2}></td>
+                    <td colSpan={2}></td>
+                    <td colSpan={2}></td>
+                </tr>
+                <tr>
+                    <th colSpan={2}>Name</th>
+                    <th rowSpan={2}>Weekly Hours</th>
+                    <th rowSpan={2}>Start</th>
+                    <th rowSpan={2}>Finish</th>
+                    <th rowSpan={2}>Start</th>
+                    <th rowSpan={2}>Finish</th>
+                    <th rowSpan={2}>Start</th>
+                    <th rowSpan={2}>Finish</th>
+                    <th rowSpan={2}>Start</th>
+                    <th rowSpan={2}>Finish</th>
+                    <th rowSpan={2}>Start</th>
+                    <th rowSpan={2}>Finish</th>
+                    <th rowSpan={2}>Start</th>
+                    <th rowSpan={2}>Finish</th>
+                    <th rowSpan={2}>Start</th>
+                    <th rowSpan={2}>Finish</th>
+                </tr>
+                <tr>
+                    <th colSpan={2}>Employee no.</th>
+                </tr>
+                <tr>
+                    <th colSpan={2}>Aisling McCurtin</th>
+                    <th rowSpan={2}></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <th colSpan={3} className="text-left">Signed:</th>
+                </tr>
+                <tr>
+                    <th colSpan={2}>ED30357</th>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td colSpan={2}></td>
+                </tr>
+                <tr>
+                    <th colSpan={2}>David Barea</th>
+                    <th rowSpan={2}></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <th colSpan={3} className="text-left">Signed:</th>
+                </tr>
+                <tr>
+                    <th colSpan={2}>ED28330</th>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td colSpan={2}></td>
+                </tr>
+                <tr>
+                    <th colSpan={2}>Dorija Glasovac</th>
+                    <th rowSpan={2}></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <th colSpan={3} className="text-left">Signed:</th>
+                </tr>
+                <tr>
+                    <th colSpan={2}>ED30357</th>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td colSpan={2}></td>
+                </tr>
+                <tr>
+                    <th colSpan={2}>Brandon Pascutoi</th>
+                    <th rowSpan={2}></th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <th colSpan={3} className="text-left">Signed:</th>
+                </tr>
+                <tr>
+                    <th colSpan={2}></th>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td></td>
+                    <th>Total:</th>
+                    <td colSpan={2}></td>
+                </tr>
+            </tbody>
         </table>
     )
 }
