7e2b1018b8fa8e7d02c27f7d97e4fa58f50150a0
unknown <brandonalexescu@yahoo.com>
2023-08-11 04:47:59 +0100

    started work on code restructure for table

--- src/components/RosterHoursTable/RosterHoursTable.js
+++ src/components/RosterHoursTable/RosterHoursTable.js
@@ -5,200 +5,29 @@ const RosterHoursTable = () => {
         <table className='mt-3 h-100 w-100 table-bordered'>
             <tbody>
                 <tr>
-                    <th colSpan={3} rowSpan={4}>Employee Information</th>
-                    <th colSpan={2} rowSpan={3}>Monday</th>
-                    <th colSpan={2} rowSpan={3}>Tuesday</th>
-                    <th colSpan={2} rowSpan={3}>Wednesday</th>
-                    <th colSpan={2} rowSpan={3}>Thursday</th>
-                    <th colSpan={2} rowSpan={3}>Friday</th>
-                    <th colSpan={2} rowSpan={3}>Saturday</th>
-                    <th colSpan={2} rowSpan={3}>Sunday</th>
-                    <th colSpan={3} rowSpan={6}>Total Work Hours</th>
+                    <th rowSpan={4}>Employee Information</th>
+                    <th rowSpan={3}>Monday</th>
+                    <th rowSpan={3}>Tuesday</th>
+                    <th rowSpan={3}>Wednesday</th>
+                    <th rowSpan={3}>Thursday</th>
+                    <th rowSpan={3}>Friday</th>
+                    <th rowSpan={3}>Saturday</th>
+                    <th rowSpan={3}>Sunday</th>
+                    <th rowSpan={6}>Total Week Hours</th>
                 </tr>
                 <tr></tr>
                 <tr></tr>
                 <tr>
-                    <td colSpan={2}></td>
-                    <td colSpan={2}></td>
-                    <td colSpan={2}></td>
-                    <td colSpan={2}></td>
-                    <td colSpan={2}></td>
-                    <td colSpan={2}></td>
-                    <td colSpan={2}></td>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
                 </tr>
-                <tr>
-                    <th colSpan={2}>Name</th>
-                    <th rowSpan={2}>Weekly Hours</th>
-                    <th rowSpan={2}>Start</th>
-                    <th rowSpan={2}>Finish</th>
-                    <th rowSpan={2}>Start</th>
-                    <th rowSpan={2}>Finish</th>
-                    <th rowSpan={2}>Start</th>
-                    <th rowSpan={2}>Finish</th>
-                    <th rowSpan={2}>Start</th>
-                    <th rowSpan={2}>Finish</th>
-                    <th rowSpan={2}>Start</th>
-                    <th rowSpan={2}>Finish</th>
-                    <th rowSpan={2}>Start</th>
-                    <th rowSpan={2}>Finish</th>
-                    <th rowSpan={2}>Start</th>
-                    <th rowSpan={2}>Finish</th>
-                </tr>
-                <tr>
-                    <th colSpan={2}>Employee no.</th>
-                </tr>
-                <tr>
-                    <th colSpan={2}>Aisling McCurtin</th>
-                    <th rowSpan={2}></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th colSpan={3} className="text-left">Signed:</th>
-                </tr>
-                <tr>
-                    <th colSpan={2}>ED30357</th>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td colSpan={2}></td>
-                </tr>
-                <tr>
-                    <th colSpan={2}>David Barea</th>
-                    <th rowSpan={2}></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th colSpan={3} className="text-left">Signed:</th>
-                </tr>
-                <tr>
-                    <th colSpan={2}>ED28330</th>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td colSpan={2}></td>
-                </tr>
-                <tr>
-                    <th colSpan={2}>Dorija Glasovac</th>
-                    <th rowSpan={2}></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th colSpan={3} className="text-left">Signed:</th>
-                </tr>
-                <tr>
-                    <th colSpan={2}>ED30357</th>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td colSpan={2}></td>
-                </tr>
-                <tr>
-                    <th colSpan={2}>Brandon Pascutoi</th>
-                    <th rowSpan={2}></th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <th colSpan={3} className="text-left">Signed:</th>
-                </tr>
-                <tr>
-                    <th colSpan={2}></th>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td></td>
-                    <th>Total:</th>
-                    <td colSpan={2}></td>
-                </tr>
+                <tr></tr>
+                <tr></tr>
             </tbody>
         </table>
     )
