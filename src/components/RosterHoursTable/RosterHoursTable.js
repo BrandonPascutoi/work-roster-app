e3fc44364fc335b1b3c87a88792cde0ae7ce37dc
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-08-14 22:29:04 +0100

    continued work on code restructure for table

--- src/components/RosterHoursTable/RosterHoursTable.js
+++ src/components/RosterHoursTable/RosterHoursTable.js
@@ -5,29 +5,204 @@ const RosterHoursTable = () => {
         <table className='mt-3 h-100 w-100 table-bordered'>
             <tbody>
                 <tr>
-                    <th rowSpan={4}>Employee Information</th>
-                    <th rowSpan={3}>Monday</th>
-                    <th rowSpan={3}>Tuesday</th>
-                    <th rowSpan={3}>Wednesday</th>
-                    <th rowSpan={3}>Thursday</th>
-                    <th rowSpan={3}>Friday</th>
-                    <th rowSpan={3}>Saturday</th>
-                    <th rowSpan={3}>Sunday</th>
-                    <th rowSpan={6}>Total Week Hours</th>
+                    <th rowSpan={4} colSpan={4}>Employee Information</th>
+                    <th rowSpan={3} colSpan={2}>Monday</th>
+                    <th rowSpan={3} colSpan={2}>Tuesday</th>
+                    <th rowSpan={3} colSpan={2}>Wednesday</th>
+                    <th rowSpan={3} colSpan={2}>Thursday</th>
+                    <th rowSpan={3} colSpan={2}>Friday</th>
+                    <th rowSpan={3} colSpan={2}>Saturday</th>
+                    <th rowSpan={3} colSpan={2}>Sunday</th>
+                    <th rowSpan={6} colSpan={4}>Total Week Hours</th>
                 </tr>
                 <tr></tr>
                 <tr></tr>
                 <tr>
+                    <th colSpan={2} className="insert-date-cell">Insert Date</th>
+                    <th colSpan={2} className="insert-date-cell">Insert Date</th>
+                    <th colSpan={2} className="insert-date-cell">Insert Date</th>
+                    <th colSpan={2} className="insert-date-cell">Insert Date</th>
+                    <th colSpan={2} className="insert-date-cell">Insert Date</th>
+                    <th colSpan={2} className="insert-date-cell">Insert Date</th>
+                    <th colSpan={2} className="insert-date-cell">Insert Date</th>
+                </tr>
+                <tr>
+                    <th rowSpan={1} colSpan={3}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                    <th rowSpan={2}></th>
+                </tr>
+                <tr>
+                    <th rowSpan={1} colSpan={3}></th>
+                </tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th rowSpan={2}></th>
                     <th></th>
                     <th></th>
                     <th></th>
                     <th></th>
                     <th></th>
                     <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={4}></th>
+                </tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={2}></th>
                     <th></th>
                 </tr>
-                <tr></tr>
-                <tr></tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th rowSpan={2}></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={4}></th>
+                </tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={2}></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th rowSpan={2}></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={4}></th>
+                </tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={2}></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th rowSpan={2}></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={4}></th>
+                </tr>
+                <tr>
+                    <th colSpan={3}></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th colSpan={2}></th>
+                    <th></th>
+                </tr>
             </tbody>
         </table>
     )
