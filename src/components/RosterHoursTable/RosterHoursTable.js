08bf3ac549f21be3e420c5ebea9637195d0aae57
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-08-16 22:44:21 +0100

    changed location of 'classname's in table to use less repeating code

--- src/components/RosterHoursTable/RosterHoursTable.js
+++ src/components/RosterHoursTable/RosterHoursTable.js
@@ -4,48 +4,48 @@ const RosterHoursTable = () => {
     return (
         <table className='mt-3 h-100 w-100 table-bordered'>
             <tbody>
-                <tr>
-                    <th rowSpan={4} colSpan={4} className='four-cell-height'>Employee Information</th>
-                    <th rowSpan={3} colSpan={2} className='three-cell-height'>Monday</th>
-                    <th rowSpan={3} colSpan={2} className='three-cell-height'>Tuesday</th>
-                    <th rowSpan={3} colSpan={2} className='three-cell-height'>Wednesday</th>
-                    <th rowSpan={3} colSpan={2} className='three-cell-height'>Thursday</th>
-                    <th rowSpan={3} colSpan={2} className='three-cell-height'>Friday</th>
-                    <th rowSpan={3} colSpan={2} className='three-cell-height'>Saturday</th>
-                    <th rowSpan={3} colSpan={2} className='three-cell-height'>Sunday</th>
-                    <th rowSpan={6} colSpan={4} className='six-cell-height'>Total Week Hours</th>
+                <tr className='two-cell-height'>
+                    <th rowSpan={4} colSpan={4}>Employee Information</th>
+                    <th rowSpan={3} colSpan={2}>Monday</th>
+                    <th rowSpan={3} colSpan={2}>Tuesday</th>
+                    <th rowSpan={3} colSpan={2}>Wednesday</th>
+                    <th rowSpan={3} colSpan={2}>Thursday</th>
+                    <th rowSpan={3} colSpan={2}>Friday</th>
+                    <th rowSpan={3} colSpan={2}>Saturday</th>
+                    <th rowSpan={3} colSpan={2}>Sunday</th>
+                    <th rowSpan={6} colSpan={4}>Total Week Hours</th>
                 </tr>
-                <tr></tr>
-                <tr></tr>
-                <tr>
-                    <th colSpan={2} className="one-cell-height">Insert Date</th>
-                    <th colSpan={2} className="one-cell-height">Insert Date</th>
-                    <th colSpan={2} className="one-cell-height">Insert Date</th>
-                    <th colSpan={2} className="one-cell-height">Insert Date</th>
-                    <th colSpan={2} className="one-cell-height">Insert Date</th>
-                    <th colSpan={2} className="one-cell-height">Insert Date</th>
-                    <th colSpan={2} className="one-cell-height">Insert Date</th>
+                <tr className='one-cell-height'></tr>
+                <tr className='one-cell-height'></tr>
+                <tr className="one-cell-height">
+                    <th colSpan={2}>&gt;Insert Date&lt;</th>
+                    <th colSpan={2}>&gt;Insert Date&lt;</th>
+                    <th colSpan={2}>&gt;Insert Date&lt;</th>
+                    <th colSpan={2}>&gt;Insert Date&lt;</th>
+                    <th colSpan={2}>&gt;Insert Date&lt;</th>
+                    <th colSpan={2}>&gt;Insert Date&lt;</th>
+                    <th colSpan={2}>&gt;Insert Date&lt;</th>
                 </tr>
-                <tr>
-                    <th rowSpan={1} colSpan={3} className="one-cell-height">Name</th>
-                    <th rowSpan={2} className="two-cell-height">Weekly Hours</th>
-                    <th rowSpan={2} className="two-cell-height">Start</th>
-                    <th rowSpan={2} className="two-cell-height">Finish</th>
-                    <th rowSpan={2} className="two-cell-height">Start</th>
-                    <th rowSpan={2} className="two-cell-height">Finish</th>
-                    <th rowSpan={2} className="two-cell-height">Start</th>
-                    <th rowSpan={2} className="two-cell-height">Finish</th>
-                    <th rowSpan={2} className="two-cell-height">Start</th>
-                    <th rowSpan={2} className="two-cell-height">Finish</th>
-                    <th rowSpan={2} className="two-cell-height">Start</th>
-                    <th rowSpan={2} className="two-cell-height">Finish</th>
-                    <th rowSpan={2} className="two-cell-height">Start</th>
-                    <th rowSpan={2} className="two-cell-height">Finish</th>
-                    <th rowSpan={2} className="two-cell-height">Start</th>
-                    <th rowSpan={2} className="two-cell-height">Finish</th>
+                <tr className="one-cell-height">
+                    <th rowSpan={1} colSpan={3}>Name</th>
+                    <th rowSpan={2} >Weekly Hours</th>
+                    <th rowSpan={2} >Start</th>
+                    <th rowSpan={2} >Finish</th>
+                    <th rowSpan={2} >Start</th>
+                    <th rowSpan={2} >Finish</th>
+                    <th rowSpan={2} >Start</th>
+                    <th rowSpan={2} >Finish</th>
+                    <th rowSpan={2} >Start</th>
+                    <th rowSpan={2} >Finish</th>
+                    <th rowSpan={2} >Start</th>
+                    <th rowSpan={2} >Finish</th>
+                    <th rowSpan={2} >Start</th>
+                    <th rowSpan={2} >Finish</th>
+                    <th rowSpan={2} >Start</th>
+                    <th rowSpan={2} >Finish</th>
                 </tr>
-                <tr>
-                    <th rowSpan={1} colSpan={3} className="one-cell-height">Employee Number</th>
+                <tr className="one-cell-height">
+                    <th rowSpan={1} colSpan={3}>Employee Number</th>
                 </tr>
                 <tr>
                     <th colSpan={3}></th>
