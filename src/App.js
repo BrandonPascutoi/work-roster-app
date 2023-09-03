a771d941f044f223e866e2c2b7d694a9ed22db00
unknown <brandonalexescu@yahoo.com>
2023-09-03 19:33:29 +0100

    added WeekStartingInput element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import GatherAndGatherLogo from './components/GatherAndGatherLogo/GatherAndGatherLogo';
+import WeekStartingInput from './components/WeekStartingInput/WeekStartingInput';
 import RosterHoursTable from './components/RosterHoursTable/RosterHoursTable';
 
 function App() {
   return (
     <div className="App h-100 d-flex flex-column align-items-center container-fluid">
       <GatherAndGatherLogo />
+      <WeekStartingInput />
       <RosterHoursTable />
     </div>
   );
