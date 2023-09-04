a195fdfe8410c0bc530466ee15f90b6abff89e48
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-09-04 22:03:24 +0100

    created submit button and started work on date input functionality for table

--- src/components/WeekStartingInput/WeekStartingInput.js
+++ src/components/WeekStartingInput/WeekStartingInput.js
@@ -2,10 +2,12 @@ import './WeekStartingInput.css'
 
 const WeekStartingInput = () => {
     return (
-        <div className='weekStartingInput w-100 mt-3'>
-            <label className='w-100 h-100 d-flex justify-content-center'>Week starting:
-            <input type='date' className='h-100 w-50 p-0' id='dateInput'></input>
+        <div className='weekStartingInput w-100 mt-3 d-flex'>
+            <label htmlFor='dateInput' className='w-75 h-100 d-flex justify-content-evenly align-items-center'>
+            Week starting:
+                <input name='dateInput' type='date' className='h-100 p-0 text-center' id='dateInput' required></input>
             </label>
+            <input type='submit'></input>
         </div>
     )
 }
