efd959b70255e049d0045a069e14a5e512486c9b
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-09-06 21:27:24 +0100

    changed component to form element and prevented default event when submit button is clicked

--- src/components/WeekStartingInput/WeekStartingInput.js
+++ src/components/WeekStartingInput/WeekStartingInput.js
@@ -1,15 +1,20 @@
-import './WeekStartingInput.css'
+import './WeekStartingInput.css';
 
 const WeekStartingInput = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        // You can handle the form submission logic here
+    };
+
     return (
-        <div className='weekStartingInput w-100 mt-3 d-flex'>
+        <form className='weekStartingInput w-100 mt-3 d-flex' onSubmit={handleSubmit}>
             <label htmlFor='dateInput' className='w-75 h-100 d-flex justify-content-evenly align-items-center'>
-            Week starting:
-                <input name='dateInput' type='date' className='h-100 p-0 text-center' id='dateInput' required></input>
+                Week starting:
+                <input name='dateInput' type='date' className='h-100 p-0 text-center' id='dateInput' required />
             </label>
-            <input type='submit'></input>
-        </div>
-    )
-}
+            <button type='submit'>Submit</button>
+        </form>
+    );
+};
 
 export default WeekStartingInput;
