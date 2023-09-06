6897e30fb34610de0b03ed049a095b77e80cebe2
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-09-06 22:04:44 +0100

    started learning React ContextAPI and experimenting with its implementation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import React from 'react';
 import './App.css'
 import GatherAndGatherLogo from './components/GatherAndGatherLogo/GatherAndGatherLogo';
 import WeekStartingInput from './components/WeekStartingInput/WeekStartingInput';
 import RosterHoursTable from './components/RosterHoursTable/RosterHoursTable';
+
+export const DateContext = React.createContext();
 
 function App() {
   return (
     <div className="App h-100 d-flex flex-column align-items-center container-fluid">
       <GatherAndGatherLogo />
       <WeekStartingInput />
-      <RosterHoursTable />
+      <DateContext.Provider value=''>
+        <RosterHoursTable />
+      </DateContext.Provider>
     </div>
   );
+}
+
+function Date() {
+  const value = React.useContext(DateContext);
+
+  return <p>{value}</p>
 }
 
 export default App;
