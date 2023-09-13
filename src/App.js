17f24ddbcb4ee083d22b0c5bf12e76cab2020765
unknown <brandonalexescu@yahoo.com>
2023-09-13 21:34:51 +0100

    restructured code for Context api use, also added state to App component (date)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React from 'react';
+import { createContext, useState } from 'react';
 import './App.css'
 import GatherAndGatherLogo from './components/GatherAndGatherLogo/GatherAndGatherLogo';
 import WeekStartingInput from './components/WeekStartingInput/WeekStartingInput';
 import RosterHoursTable from './components/RosterHoursTable/RosterHoursTable';
 
-export const DateContext = React.createContext();
+export const DateContext = createContext("");
 
 function App() {
+  const [date, setDate] = useState("");
+
   return (
     <div className="App h-100 d-flex flex-column align-items-center container-fluid">
       <GatherAndGatherLogo />
       <WeekStartingInput />
-      <DateContext.Provider value=''>
+      <DateContext.Provider value={{ date, setDate }}>
         <RosterHoursTable />
       </DateContext.Provider>
     </div>
   );
-}
-
-function Date() {
-  const value = React.useContext(DateContext);
-
-  return <p>{value}</p>
 }
 
 export default App;
