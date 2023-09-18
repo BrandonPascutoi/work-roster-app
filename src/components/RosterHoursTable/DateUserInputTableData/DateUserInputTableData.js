209dc46d09c2d88af82e3a61e78bece4e784ebaf
unknown <brandonalexescu@yahoo.com>
2023-09-18 21:49:46 +0100

    imported context and used it in component

--- src/components/RosterHoursTable/DateUserInputTableData/DateUserInputTableData.js
+++ src/components/RosterHoursTable/DateUserInputTableData/DateUserInputTableData.js
@@ -1,7 +1,10 @@
 import { useContext } from "react";
+import { MyContext } from "../../../myContext";
 import './DateUserInputTableData.css'
 
 const DateInputCell = props => {
+    const { date, setDate } = useContext(MyContext);
+
     return (
         <td rowSpan={props.rowSpan} colSpan={props.colSpan} className='light-blue date'>&gt;Date&lt;</td>
     )
