ce2052e95c51537649449c671b6c68d692c80ceb
unknown <brandonalexescu@yahoo.com>
2023-11-14 17:47:33 +0000

    started work on calculate button for table

---
+++ src/components/CalculateButton/CalculateButton.js
@@ -0,0 +1,9 @@
+import './CalculateButton.css'
+
+const CalculateButton = () => {
+    return (
+        <button className='calculateButton position-relative'>Calculate</button>
+    )
+}
+
+export default CalculateButton;
