39f2bc7c7248b935cbba6695e5bcda8f7f7e3a39
unknown <brandonalexescu@yahoo.com>
2023-11-30 18:07:19 +0000

    replaced more <th> elements with seperate react components

--- src/components/RosterHoursTable/TableRow/TableRow.js
+++ src/components/RosterHoursTable/TableRow/TableRow.js
@@ -1,16 +1,8 @@
 import './TableRow.css';
-import TableHeader from './TableHeader/TableHeader';
 
-const TableRow = () => {
+const TableRow = ({ children, className }) => {
     return (
-        <tr>
-            <TableHeader rowSpan={4} colSpan={4} className="dark-brown" value='Employee Information'/>
-            <TableHeader rowSpan={2} colSpan={2} value='Name:'/>
-            <TableHeader rowSpan={2} colSpan={4} value='Aisling McCurtin'/>
-            <TableHeader rowSpan={2} colSpan={4} value='Brandon Pascutoi'/>
-            <TableHeader rowSpan={2} colSpan={4} value='Mahan Salavati'/>
-            <TableHeader rowSpan={2} colSpan={4} value='Anthony Cheung'/>
-        </tr>
+        <tr className={className}>{children}</tr>
     )
 }
 
