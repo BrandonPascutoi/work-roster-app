6bada6359b328a357c5a16f7c2ab09a90af45ea8
unknown <brandonalexescu@yahoo.com>
2023-12-12 19:35:17 +0000

    started work on implementing components functionality to track start and finish times for each cell using state

--- src/components/RosterHoursTable/TimeUserInputTableData/TimeUserInputTableData.js
+++ src/components/RosterHoursTable/TimeUserInputTableData/TimeUserInputTableData.js
@@ -1,22 +1,44 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './TimeUserInputTableData.css'
+import { MyContext } from '../../../myContext';
+
+const timeInputCellsArrayAndStates = () => {
+  
+}
 
 function TimeInputCell(props) {
   const [inputValue, setInputValue] = useState('');
+  const {startTimes, finishTimes} = useContext(MyContext);
+
+  const [startTimesValue, setStartTimeValue] = startTimes;
+  const [finishTimesValue, setFinishTimeValue] = finishTimes;
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  const timeInputCells = [];
+
+  for (let i = 0; i < 56; i++) {
+    timeInputCells.push(
+      <td rowSpan={props.rowSpan} colSpan={props.colSpan}>
+        <input
+          type="time"
+          value={inputValue}
+          onChange={handleInputChange}
+          className={`timeInputCell${i} h-100 w-100 d-flex justify-content-between align-items-center`}
+          id='timeInputCell'
+        />
+      </td>
+    )
+    //setStartTimeValue({ ...startTimesValue, })
+  }
+
+  console.log(timeInputCells);
+  console.log(startTimesValue);
+
   return (
-    <td rowSpan={props.rowSpan} colSpan={props.colSpan} className={props.className}>
-      <input
-        type="time"
-        value={inputValue}
-        onChange={handleInputChange}
-        className='timeInputCell h-100 w-100 d-flex justify-content-between align-items-center'
-      />
-    </td>
+    timeInputCells[props.timeInputCellArrayIndex]
   );
 }
 
